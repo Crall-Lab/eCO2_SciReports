325f348496b93d3b3112cabcb2c80e1a8dfc7911
ombernauer <97457642+ombernauer@users.noreply.github.com>
2024-02-02 11:53:53 -0600

    Update Plant_growth.R

--- Plant_growth.R
+++ Plant_growth.R
@@ -57,6 +57,8 @@ flowers <- W.plant%>% filter(!is.na(Flower_no))
 W.FF$Date <- as.Date(W.FF$Date, "%m/%d/%y")
 W.FF <- W.FF %>% filter(!is.na(Date))
 W.FF$Chamber <- factor(W.FF$Chamber, levels = c(60, 63, 62, 61))
+W.FF$CO2 <- as.factor(W.FF$CO2)
+W.FF$Plant <- as.factor(W.FF$Plant)
 
 
 ## flower area
@@ -68,7 +70,7 @@ W.area$CO2 <- as.factor(W.area$CO2)
 biomass$CO2 <- as.factor(biomass$CO2)
 biomass$Plant <- as.factor(biomass$Plant)
 
-############################
+###################
 
 ## height
 
@@ -393,176 +395,76 @@ m.3 <- glmer.nb(Flower_no ~ CO2*Plant+Round+datenum+ (1|Chamber),
 plot(simulationOutput <- simulateResiduals(fittedModel=m.B2, plot =F))
 car::Anova(m.3) # report this
 
-######
+# compare each plant species separately 
 ## Borage
-#######
-h.B <- flowers %>% filter(Plant == "B")
+f.B <- flowers %>% filter(Plant == "B")
 
-hist(h.B$Flower_no) # zero-inflated
 m.B2 <- glmer.nb(Flower_no ~ CO2 + Round + datenum + (1|Chamber),
-                 data = h.B)
+                 data = f.B)
 plot(simulationOutput <- simulateResiduals(fittedModel=m.B2, plot =F))
-#check for overdispersion
-overdisp_fun(m.B2) # yes overdispersion - wait actual underdispersion
-testDispersion(m.B2, alternative="less")
 car::Anova(m.B2) # report this
-AIC(m.B2) # lower AIC than lmer, so go with this. 
 
-#######
 ## Buckwheat
-#########
-h.BW <- flowers %>% filter(Plant == "BW")
+f.BW <- flowers %>% filter(Plant == "BW")
 
-m.BW1 <- lmer(Flower_no ~ CO2+Round+datenum + (1|Chamber), data = h.BW, REML=F)
-plot(simulationOutput <- simulateResiduals(fittedModel=m.BW1, plot =F))
-summary(m.BW1) #AIC 9860.9
-anova(m.BW1)
-# significant difference by CO2 level
-hist(h.BW$Flower_no) # zero-inflated
 m.BW2 <- glmer.nb(Flower_no ~ CO2 + Round + datenum + (1|Chamber),
-                  data = h.BW)
+                  data = f.BW)
 plot(simulationOutput <- simulateResiduals(fittedModel=m.BW2, plot =F))
-#check for overdispersion
-testDispersion(m.BW2, alternative="greater") # no over or under dispersion according to this test
 car::Anova(m.BW2) # report this
-AIC(m.BW2) # 7291.22 lower AIC than lmer, so go with this. 
-summary(m.BW2)
 
-
-########
 ## Clover
-########
-h.C <- flowers %>% filter(Plant == "C")
+f.C <- flowers %>% filter(Plant == "C")
 
-m.C1 <- lmer(Flower_no ~ CO2+Round+datenum + (1|Chamber), data = h.C, REML=F)
-plot(simulationOutput <- simulateResiduals(fittedModel=m.C1, plot =F))
-summary(m.C1) #AIC 8705.2
-anova(m.C1)
-# sig effect of CO2 - fewer flowers in eco2
-hist(h.C$Flower_no) # zero-inflated
 m.C2 <- glmer.nb(Flower_no ~ CO2 + Round + datenum + (1|Chamber),
-                 data = h.C)
+                 data = f.C)
 plot(simulationOutput <- simulateResiduals(fittedModel=m.C2, plot =F))
-#check for overdispersion
-testDispersion(m.C2, alternative="less") # underdispersion according to this test
 car::Anova(m.C2) # report this
-AIC(m.C2) # 6239.437 lower AIC than lmer, so go with this. 
-summary(m.C2)
 
-########
-## Dandelion - never flowered
-#########
-
-#######
 ## Lacy Phacelia
-#######
-h.LP <- flowers %>% filter(Plant == "LP")
+f.LP <- flowers %>% filter(Plant == "LP")
 
-m.LP1 <- lmer(Flower_no ~ CO2+Round+datenum + (1|Chamber), data = h.LP, REML=F)
-plot(simulationOutput <- simulateResiduals(fittedModel=m.LP1, plot =F))
-summary(m.LP1) #AIC 17846.7
-anova(m.LP1)
-# sig effect of CO2 - fewer flowers in eco2
-hist(h.LP$Flower_no) # zero-inflated
 m.LP2 <- glmer.nb(Flower_no ~ CO2 + Round + datenum + (1|Chamber),
-                  data = h.LP)
+                  data = f.LP)
 plot(simulationOutput <- simulateResiduals(fittedModel=m.LP2, plot =F))
-#check for overdispersion
-testDispersion(m.LP2, alternative="greater") # no over or underdispersion according to this test
 car::Anova(m.LP2) # report this
-AIC(m.LP2) # 13485.57 lower AIC than lmer, so go with this. 
-summary(m.LP2)
-# No significant difference by CO2 level, only round and date
 
-#########
 ## Nasturtium
-#########
-h.N <- flowers %>% filter(Plant == "N")
+f.N <- flowers %>% filter(Plant == "N")
 
-m.N1 <- lmer(Flower_no ~ CO2 + Round+ datenum + (1|Chamber), data = h.N, REML=F)
-plot(simulationOutput <- simulateResiduals(fittedModel=m.N1, plot =F))
-summary(m.N1) #AIC 6432.1
-anova(m.N1)
-# sig effect of CO2 - fewer flowers in eco2
-hist(h.N$Flower_no) # zero-inflated
 m.N2 <- glmer.nb(Flower_no ~ CO2 + Round + datenum + (1|Chamber),
-                 data = h.N)
+                 data = f.N)
 plot(simulationOutput <- simulateResiduals(fittedModel=m.N2, plot =F))
-#check for overdispersion
-testDispersion(m.N2, alternative="less") # no over or underdispersion according to this test
 car::Anova(m.N2) # report this
-AIC(m.N2) # 5037.598 lower AIC than lmer, so go with this. 
-summary(m.N2)
 
-########
-## PP
-########
-h.PP <- flowers %>% filter(Plant == "PP")
-h.PP <- h.PP %>% filter(Round == "1")
+## Partridge pea
+f.PP <- flowers %>% filter(Plant == "PP")
+f.PP <- f.PP %>% filter(Round == "1")
 
-m.PP1 <- lmer(Flower_no ~ CO2 + datenum + (1|Chamber), data = h.PP, REML=F)
-plot(simulationOutput <- simulateResiduals(fittedModel=m.PP1, plot =F))
-summary(m.PP1) #AIC 851.6
-anova(m.PP1)
-# sig effect of CO2 - fewer flowers in eco2
-hist(h.PP$Flower_no) # zero-inflated
 m.PP2 <- glmer.nb(Flower_no ~ CO2 + datenum + (1|Chamber),
                   data = h.PP)
 plot(simulationOutput <- simulateResiduals(fittedModel=m.PP2, plot =F))
-#check for overdispersion
-testDispersion(m.PP2, alternative="greater") # no over or underdispersion according to this test
 car::Anova(m.PP2) # report this
-AIC(m.PP2) # 130.2388 lower AIC than lmer, so go with this. 
-summary(m.PP2)
 
-#########
-## SA
-#########
-h.SA <- flowers %>% filter(Plant == "SA")
+## Sweet alyssum 
+f.SA <- flowers %>% filter(Plant == "SA")
 
-m.SA1 <- lmer(Flower_no ~ CO2+ Round+datenum + (1|Chamber), data = h.SA, REML=F)
-plot(simulationOutput <- simulateResiduals(fittedModel=m.SA1, plot =F))
-summary(m.SA1) #AIC 13594.0
-anova(m.SA1)
-# sig effect of CO2 - fewer flowers in eco2
-hist(h.SA$Flower_no) # zero-inflated
 m.SA2 <- glmer.nb(Flower_no ~ CO2 + Round + datenum + (1|Chamber),
-                  data = h.SA)
+                  data = f.SA)
 plot(simulationOutput <- simulateResiduals(fittedModel=m.SA2, plot =F))
-#check for overdispersion
-testDispersion(m.SA2, alternative="greater") # no over or underdispersion according to this test
 car::Anova(m.SA2) # report this
-AIC(m.SA2) # 8349.376 lower AIC than lmer, so go with this. 
-summary(m.SA2)
 
-#######
-## SF
-#######
-h.SF <- flowers %>% filter(Plant == "SF")
+## Sunflower
+f.SF <- flowers %>% filter(Plant == "SF")
 
-m.SF1 <- lmer(Flower_no ~ CO2+Round+datenum + (1|Chamber), data = h.SF, REML=F)
-plot(simulationOutput <- simulateResiduals(fittedModel=m.SF1, plot =F))
-summary(m.SF1) #AIC 461.4
-anova(m.SF1)
-# sig effect of CO2 - fewer flowers in eco2
-hist(h.SF$Flower_no) # zero-inflated
 m.SF2 <- glmer.nb(Flower_no ~ CO2 + Round + datenum + (1|Chamber),
-                  data = h.SF)
+                  data = f.SF)
 plot(simulationOutput <- simulateResiduals(fittedModel=m.SF2, plot =F))
-#check for overdispersion
-testDispersion(m.SF2, alternative="less") # no over or underdispersion according to this test
 car::Anova(m.SF2) # report this
-AIC(m.SA2) # 8349.376 lower AIC than lmer, so go with this. 
-summary(m.SA2)
-summary(m.SF1)
-anova(m.SF1)
-# No significant difference by CO2 level, only round and date
 
-###############
+############
 ## days to first flower
-##############
-# remove any plant, date, round, chambers w/o 3+ observations
 
+# remove any plant, date, round, chambers w/o 3+ observations
 W.FF <- W.FF %>% group_by(Plant, Chamber, Round) %>% filter(n()>2) %>% ungroup()
 ff.1 <- W.FF %>% filter(Round == "1")
 start.date <- parse_date_time('2021-11-17 00:00:00', "%Y-%m-%d %H:%M:%S")
@@ -574,35 +476,6 @@ ff.2$datenum <- as.numeric(difftime(parse_date_time(ff.2$Date, 'ymd'),start.date
 
 ff<- rbind(ff.1, ff.2)
 
-plants <- c("Borage", "Buckwheat","Red Clover", "Lacy Phacelia", "Nasturtium", "Partridge Pea", "Sweet Alyssum", "Sunflower")
-names(plants) <- c("B", "BW", "C", "LP", "N", "PP", "SA", "SF")
-
-## summarize and plot
-ff.sum <- ff %>% group_by(Plant, CO2) %>%
-  dplyr::summarise(
-    count = n(),
-    mean = mean(datenum, na.rm = T),
-    sd = sd(datenum, na.rm = T)
-  )
-ff.sum$se <- ff.sum$sd/sqrt(ff.sum$count)
-
-ff.sum$CO2 <- factor(ff.sum$CO2, levels = c(0, 1))
-
-ggplot(ff.sum, aes(x=Plant, y = mean, color = CO2))+
-  geom_point(position=position_dodge(0.75), size = 2)+
-  geom_errorbar(aes(ymin=mean-se, ymax = mean+se), width=0.2, position=position_dodge(0.75))+
-  theme_classic()+
-  theme(legend.position = c(0.1, 0.9),
-        axis.text.x = element_text(angle = 90))+
-  labs(y="Days since planting ± se")+
-  scale_color_manual(values = c("grey","navyblue"), 
-                     labels = c("aCO2", "eCO2"),
-                     name = "Treatment")+
-  scale_x_discrete(labels = c(plants))
-
-W.FF$CO2 <- as.factor(W.FF$CO2)
-W.FF$Plant <- as.factor(W.FF$Plant)
-
 ggplot(ff, aes(x=Plant, y = log10(datenum), fill = CO2))+
   geom_violin()+
   geom_point(position=position_jitterdodge(), size = 0.5, alpha = 0.5, aes(color=CO2))+
@@ -613,7 +486,7 @@ ggplot(ff, aes(x=Plant, y = log10(datenum), fill = CO2))+
   scale_color_manual(values = c("black", "navy"),
                      labels = c("aCO2", "eCO2"),
                      name = "CO2 Treatment")+
-  scale_x_discrete(labels = plants)+
+  scale_x_discrete(labels = plants.flowers)+
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 90, hjust=1, size = .2),
         axis.title.x=element_blank())+
@@ -625,7 +498,7 @@ ggplot(ff, aes(x=Plant, y = log10(datenum), fill = CO2))+
                position=position_dodge(0.9))+
   labs(y = "log(Days since planting)")
 
-## Compare all 
+## Compare all plant species together
 hist(log(ff$datenum))
 m.B1 <- lmer(datenum ~ CO2*Plant+Round + (1|Chamber), data = ff, REML=F)
 plot(simulationOutput <- simulateResiduals(fittedModel = m.B1, plot = F))
