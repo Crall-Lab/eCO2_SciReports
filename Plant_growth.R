cf15a54e65dfcc5e17076358f4dfae4c11b30e3d
ombernauer <97457642+ombernauer@users.noreply.github.com>
2024-05-07 16:11:26 -0500

    Update Plant_growth.R

--- Plant_growth.R
+++ Plant_growth.R
@@ -218,7 +218,26 @@ ggplot(flowers, aes(x=week, y = Flower_no, color = CO2))+
 # full model w/all plant species
 m.3 <- glmer.nb(Flower_no ~ CO2*Plant+Round+Chamber + (1|UniqueID) + (1|week),
                 data = flowers.1,
-                glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 100000)))
+                glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 2e5)))
+
+
+# rescale and center continuous parameters
+numcols <- grep("^c\\.", names(flowers.1))
+dfs <- flowers.1
+dfs[,numcols] <- scale(dfs[,numcols])
+m1_sc <- update(m.3, data = dfs)
+
+anova(m.3, m1_sc)
+car::Anova(m.3)
+car::Anova(m1_sc) # ok findings between models are not different
+AIC(m.3)
+AIC(m1_sc) # 0.01 improvement on AIC
+
+# check singularity
+tt <- getME(m1_sc,"theta")
+ll <- getME(m1_sc,"lower")
+min(tt[ll==0])
+
 plot(simulationOutput <- simulateResiduals(fittedModel=m.3, plot =F))
 car::Anova(m.3) # report this
 
